53f351db8bbe309517e19068f12e75bbe109ca4a
eltoncasacio <eltoncasacio@hotmail.com>
2021-12-08 22:48:07 -0300

    chore: :heavy_plus_sign: add react-native link react-native-vector-icons
    dependencia para funcionar os icones - liker

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import {Text} from 'react-native-elements';
 import * as S from './styles';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Input} from 'react-native-elements';
+
 const SignIn = () => {
   return (
     <S.Wrapper>
       <Text h1>Login</Text>
+      <Input
+        autoCompleteType={false}
+        placeholder="INPUT WITH CUSTOM ICON"
+        leftIcon={<Icon name="user" size={24} color="black" />}
+      />
     </S.Wrapper>
   );
 };
